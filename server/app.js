80690b49bd60e7b79e30ac5576993fefcf239af5
Merve Kabakci <merve.kabakci@smail.th-koeln.de>
2020-06-03 00:59:20 +0200

    basic server setup with logging, basic error handling and cors handling

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// database setup
+mongoose.connect('mongodb://localhost:27017/db_finder',
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+// logging setup
+app.use(morgan('dev'));
+
+// cors handling
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+    next();
+});
+
+// error handling
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+// error
+app.use((error, req, res) => {
+    res.status(error.status || 500);
+    res.json({
+        message: error.message
+    });
+});
+
+
+module.exports = app;
