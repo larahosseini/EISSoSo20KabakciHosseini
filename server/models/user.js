0cf87a84c1ff914b01fd7ca8d58c598939f3e61a
Merve Kabakci <merve.kabakci@smail.th-koeln.de>
2020-06-03 21:12:56 +0200

    added unique key

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: {type: String, required: true, lowercase: true},
+    email: {type: String, required: true, lowercase: true, unique: true},
     password: {type: String, required: true},
     verified: {type: Boolean},
     address: {
