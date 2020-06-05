61f4353d1122fd80c1fa3839ee648522ef27653d
Merve Kabakci <merve.kabakci@smail.th-koeln.de>
2020-06-05 22:09:13 +0200

    added comments

--- server/api/middleware/check_auth.js
+++ server/api/middleware/check_auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
-// prüft ob der user eingeloggt hat
+// prüft ob der user eingeloggt ist
 module.exports = (req, res, next) => {
     try {
+        // jwt token aus dem header ziehen: token sieht so aus : "Bearer token", deshalb splitten
         const token = req.headers.authorization.split(' ')[1];
         req.userData = jwt.verify(token, process.env.JWT_SECRET_KEY);
         next();
