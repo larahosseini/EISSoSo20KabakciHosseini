7f0e9927053349decb9265bb3f6b6a1868bf33ee
Merve Kabakci <merve.kabakci@smail.th-koeln.de>
2020-06-06 02:13:11 +0200

    utility class for sending emails

---
+++ server/api/utils/email_sender.js
@@ -0,0 +1,28 @@
+const nodeMailer = require('nodemailer');
+
+const transporter = nodeMailer.createTransport(
+    {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PW
+        }
+    }
+);
+
+exports.sendRegistrationEmail = function sendRegistrationEmail(email, activationLink) {
+    transporter.sendMail({
+        from: 'Support <process.env.EMAIL>',
+        to: email,
+        subject: 'Helper - Activation of your Account',
+        text: 'Please follow the link to activate your account:\n' + activationLink
+    }, function (error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + result.email);
+        }
+    });
+}
