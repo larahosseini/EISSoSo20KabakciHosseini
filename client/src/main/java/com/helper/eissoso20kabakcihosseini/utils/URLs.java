f7ba0865a45ae7a84c60a6c3e583b4300aa19a37
larahosseini <33312802+larahosseini@users.noreply.github.com>
2020-06-27 20:02:02 +0200

    helper class for urls

---
+++ client/src/main/java/com/helper/eissoso20kabakcihosseini/utils/URLs.java
@@ -0,0 +1,9 @@
+package com.helper.eissoso20kabakcihosseini.utils;
+
+public class URLs {
+
+    public static String CREATE_USER = "http://localhost:3000/api/users/signup";
+    public static String LOGIN = "http://localhost:3000/login";
+    public static String GET_USERS = "http://locahost:3000/api/users";
+
+}
