f589cbf72b6a39676bd283c996442df653c2afae
larahosseini <33312802+larahosseini@users.noreply.github.com>
2020-06-30 20:22:27 +0200

    dashboard controller and layout

--- client/src/main/java/com/helper/eissoso20kabakcihosseini/controllers/DashboardController.java
+++ client/src/main/java/com/helper/eissoso20kabakcihosseini/controllers/DashboardController.java
@@ -1,9 +1,11 @@
 package com.helper.eissoso20kabakcihosseini.controllers;
 
+import com.helper.eissoso20kabakcihosseini.App;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,6 +16,20 @@ public class DashboardController implements Initializable {
 
     @FXML
     private JFXButton tasksButton;
+
+    @FXML
+    private void openProfilePage(){
+        try {
+            App.setRoot("profile");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openTasksPage(){
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
