bc0f193a06632d09c745190a8e9cc4631a5601d5
larahosseini <33312802+larahosseini@users.noreply.github.com>
2020-06-30 20:23:02 +0200

    ProfileController handle back button

--- client/src/main/java/com/helper/eissoso20kabakcihosseini/controllers/ProfileController.java
+++ client/src/main/java/com/helper/eissoso20kabakcihosseini/controllers/ProfileController.java
@@ -1,18 +1,16 @@
 package com.helper.eissoso20kabakcihosseini.controllers;
 
+import com.helper.eissoso20kabakcihosseini.App;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
-import javafx.application.Platform;
+import com.jfoenix.controls.JFXToggleButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ResourceBundle;
 
 public class ProfileController implements Initializable {
@@ -21,20 +19,29 @@ public class ProfileController implements Initializable {
     private JFXTextField emailField, cityField, streetField, streetNumberField, zipcodeField;
 
     @FXML
-    private JFXButton updateButton, createTaskButton;
+    private JFXButton updateButton, passwordResetButton;
 
     @FXML
-    private void updateUserData(){
+    private JFXToggleButton editButton;
 
+    @FXML
+    private Text usernameText;
+
+    @FXML
+    private void updateProfile() {
     }
 
     @FXML
-    private void createTask(){
-
+    private void handleBackButton() {
+        try {
+            App.setRoot("dashboard");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        updateButton.visibleProperty().bind(editButton.selectedProperty());
     }
 }
