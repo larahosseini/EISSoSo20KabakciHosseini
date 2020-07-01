79765337d29764f5704ddd492241d0d474da8259
larahosseini <33312802+larahosseini@users.noreply.github.com>
2020-07-01 16:01:52 +0200

    fixed url

--- client/src/main/java/com/helper/eissoso20kabakcihosseini/utils/URLs.java
+++ client/src/main/java/com/helper/eissoso20kabakcihosseini/utils/URLs.java
@@ -4,6 +4,5 @@ public class URLs {
 
     public static String CREATE_USER = "http://localhost:3000/api/users/signup";
     public static String LOGIN = "http://localhost:3000/login";
-    public static String GET_USERS = "http://locahost:3000/api/users";
-
+    public static String GET_USERS = "http://localhost:3000/api/users";
 }
