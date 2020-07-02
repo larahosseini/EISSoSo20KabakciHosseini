cb68a2b7b2a7447de97696456c106504de03e17f
larahosseini <33312802+larahosseini@users.noreply.github.com>
2020-07-02 22:00:52 +0200

    added delete information with alert and fixed error

--- client/src/main/java/com/helper/eissoso20kabakcihosseini/utils/AlertHelper.java
+++ client/src/main/java/com/helper/eissoso20kabakcihosseini/utils/AlertHelper.java
@@ -1,23 +1,29 @@
 package com.helper.eissoso20kabakcihosseini.utils;
 
 import com.helper.eissoso20kabakcihosseini.App;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public class AlertHelper {
 
     public static Alert alert = new Alert(Alert.AlertType.NONE);
     public static Stage stage = new Stage();
 
-    public static void showErrorAlert(String error){
+    public static void showErrorAlert(String error) {
+        alert = new Alert(Alert.AlertType.ERROR);
         alert.initModality(Modality.APPLICATION_MODAL);
-        alert.setAlertType(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Es ist ein Fehler aufgetreten");
         alert.setContentText("Fehler: " + error);
@@ -25,16 +31,15 @@ public class AlertHelper {
     }
 
     public static void showSuccesAlert(String headerText, String message) {
-        System.out.println(message);
+        alert = new Alert(Alert.AlertType.INFORMATION);
         alert.initModality(Modality.APPLICATION_MODAL);
-        alert.setAlertType(Alert.AlertType.INFORMATION);
         alert.setTitle("Success");
         alert.setHeaderText(headerText);
         alert.setContentText(message);
         alert.showAndWait();
     }
 
-    public static void showLoadingDialog(){
+    public static void showLoadingDialog() {
         try {
             Parent dialog = App.loadFXML("loading");
             stage.setScene(new Scene(dialog));
@@ -44,6 +49,29 @@ public class AlertHelper {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void showWarningBeforeDeleteAlert(String email, String id) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.setTitle("Warnung");
+        alert.setHeaderText("Account löschen");
+        alert.setContentText("Bitte bestätigen Sie die Löschung Ihres Accounts");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK && SessionHandler.deleteSession() && Data.deleteUserData(email)) {
+            Task<Void> task = APICaller.deleteAccount(id);
+            task.messageProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                    AlertHelper.showSuccesAlert("Löschung durchgeführt", "Dein Account wurde erfolgreich gelöscht.");
+                    try {
+                        App.setRoot("login");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            new Thread(task).start();
+        }
     }
 }
